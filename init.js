975a315d0c0de363f2a11b7e75d19b9425ad13a4
redballoon <fred@Edmeisters-MacBook-Air.local>
2015-04-05 01:32:41 -0400

    edits

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 var debug = true;
 var http = require('http');
 
+if (debug) console.log('server: will create server');
 http.createServer(function (request, response) {
 	if (debug) console.log('server: success', request, response);
 	
@@ -8,3 +9,5 @@ http.createServer(function (request, response) {
 	response.write('hello you');
 	response.end();
 }).listen(8080);
+
+if (debug) console.log('server: end');
